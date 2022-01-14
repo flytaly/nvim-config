e6d521247676c59a17b746e0c8beaf4b66ad0e5c
Vitaly Yerofeyevksy <flytaly@gmail.com>
2022-01-14 04:09:59 +0300

    Fix React go to definition

--- lua/config/lsp/servers/tsserver.lua
+++ lua/config/lsp/servers/tsserver.lua
@@ -1,8 +1,44 @@
 local ts_utils = require("nvim-lsp-ts-utils")
 
+local function filter(arr, fn)
+	if type(arr) ~= "table" then
+		return arr
+	end
+
+	local filtered = {}
+	for k, v in pairs(arr) do
+		if fn(v, k, arr) then
+			table.insert(filtered, v)
+		end
+	end
+
+	return filtered
+end
+
+local function filterReactDTS(value)
+	return string.match(value.uri, "react/index.d.ts") == nil
+end
+
+-- Filter react definition files after using vim.lsp.buf.definition()
+-- preventing opening quickfix window.
+-- https://github.com/typescript-language-server/typescript-language-server/issues/216#issuecomment-1005272952
+local handlers = {
+	["textDocument/definition"] = function(err, result, method, ...)
+		if vim.tbl_islist(result) and #result > 1 then
+			local filtered_result = filter(result, filterReactDTS)
+			return vim.lsp.handlers["textDocument/definition"](err, filtered_result, method, ...)
+		end
+
+		vim.lsp.handlers["textDocument/definition"](err, result, method, ...)
+	end,
+}
+
 return function(on_attach)
 	return {
 		init_options = require("nvim-lsp-ts-utils").init_options,
+
+		handlers = handlers,
+
 		on_attach = function(client, bufnr)
 			client.resolved_capabilities.document_formatting = false
 			client.resolved_capabilities.document_range_formatting = false
