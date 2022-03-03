33855b37b52073bcdf5022084e640efce470d151
Vitaly Yerofeyevksy <flytaly@gmail.com>
2022-03-03 20:41:47 +0300

    Gitsigns keymaps

--- lua/config/plugins/init.lua
+++ lua/config/plugins/init.lua
@@ -48,8 +48,9 @@ return require("packer").startup(function(use)
 
 	use({
 		"lewis6991/gitsigns.nvim",
+		requires = { "nvim-lua/plenary.nvim" },
 		config = function()
-			require("gitsigns").setup()
+			require("config.plugins.gitsigns")
 		end,
 	})
 
