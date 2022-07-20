ba571078b4612d2aa0151690241ccf070a1b7c75
Vitaly Yerofeyevsky <flytaly@gmail.com>
2022-07-20 18:04:45 +0300

    Use one char jump for HOP

--- lua/config/plugins/hop.lua
+++ lua/config/plugins/hop.lua
@@ -1,6 +1,6 @@
 require("hop").setup({
 	jump_on_sole_occurrence = true,
 })
-vim.keymap.set("n", "<Leader>w", "<cmd>HopChar2<CR>")
+vim.keymap.set("n", "<Leader>w", "<cmd>HopChar1<CR>")
 vim.keymap.set("n", "<Leader>j", "<cmd>HopLineStartAC<CR>")
 vim.keymap.set("n", "<Leader>k", "<cmd>HopLineStartBC<CR>")
