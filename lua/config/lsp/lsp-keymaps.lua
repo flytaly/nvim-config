3aa5e7b9b3b180788dae16b52783b123c781d0e4
Vitaly Yerofeyevsky <flytaly@gmail.com>
2022-11-02 15:55:55 +0300

    Update lsp keymaps

--- lua/config/lsp/lsp-keymaps.lua
+++ lua/config/lsp/lsp-keymaps.lua
@@ -1,36 +1,30 @@
 local format = require("config.lsp.format")
 
 local function set_default_keymaps(_, bufnr)
-	local function keymap(...)
-		vim.api.nvim_buf_set_keymap(bufnr, ...)
-	end
-
 	-- Mappings.
-	local opts = { noremap = true, silent = true }
+	local opts = { noremap = true, silent = true, buffer = bufnr }
 
 	-- See `:help vim.lsp.*` for documentation on any of the below functions
-	keymap("n", "ga", "<cmd>CodeActionMenu<CR>", opts)
-	keymap("v", "ga", "<cmd>CodeActionMenu<CR>", opts)
-	keymap("n", "<space>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
-	keymap("v", "<space>ca", ":'<,'>lua vim.lsp.buf.range_code_action()<CR>", opts)
-	keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
-	keymap("n", "<C-t>gd", ":tab split<CR>:lua vim.lsp.buf.definition()<CR>", opts)
-	keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
-	keymap("n", "gr", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
+	vim.keymap.set({ "n", "v" }, "ga", "<cmd>CodeActionMenu<CR>", opts)
+	vim.keymap.set("n", "<space>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
+	vim.keymap.set("v", "<space>ca", ":'<,'>lua vim.lsp.buf.range_code_action()<CR>", opts)
+	vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
+	vim.keymap.set("n", "<C-t>gd", ":tab split<CR>:lua vim.lsp.buf.definition()<CR>", opts)
+	vim.keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+	vim.keymap.set("n", "gr", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
 	-- use Trouble for references
-	-- keymap("n", "gR", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
-	keymap("n", "g[", "<cmd>lua vim.diagnostic.goto_prev()<CR>", opts)
-	keymap("n", "g]", "<cmd>lua vim.diagnostic.goto_next()<CR>", opts)
-	keymap("n", "gD", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
-	keymap("n", "<leader>gd", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+	-- vim.keymap.set("n", "gR", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
+	vim.keymap.set("n", "g[", "<cmd>lua vim.diagnostic.goto_prev()<CR>", opts)
+	vim.keymap.set("n", "g]", "<cmd>lua vim.diagnostic.goto_next()<CR>", opts)
+	vim.keymap.set("n", "gD", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
+	vim.keymap.set("n", "<leader>gd", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
 
 	vim.keymap.set("n", "<leader>gf", format.format, opts)
 
-	keymap("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
-	keymap("n", "<C-p>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
-	keymap("i", "<C-p>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
+	vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
+	vim.keymap.set({ "i", "n" }, "<C-'>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
 
-	keymap("n", "<leader>fls", "<cmd>Telescope lsp_workspace_symbols<CR>", opts)
+	vim.keymap.set("n", "<leader>fls", "<cmd>Telescope lsp_workspace_symbols<CR>", opts)
 end
 
 return {
