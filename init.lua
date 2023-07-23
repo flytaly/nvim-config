33f10b8ff7fc2e24f88b69191ab392e0df3f4a67
Vitaly Yerofeyevsky <flytaly@gmail.com>
2023-07-23 10:18:19 +0300

    Skip if vscode

--- init.lua
+++ init.lua
@@ -1,9 +1,12 @@
-require("config.mapping")
-require("config.lazy-nvim")
-require("config.options")
-require("config.filetypes")
-require("config.theme")
-require("config.lsp")
-require("config.debug")
-require("neovide")
-require("eslint")
+if vim.g.vscode then
+else
+	require("config.mapping")
+	require("config.lazy-nvim")
+	require("config.options")
+	require("config.filetypes")
+	require("config.theme")
+	require("config.lsp")
+	require("config.debug")
+	require("neovide")
+	require("eslint")
+end
