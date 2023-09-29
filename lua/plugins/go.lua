50b0f4a8d32b31f70edd7d76ead40855c85afb1f
Vitaly Yerofeyevsky <flytaly@gmail.com>
2023-09-29 17:51:53 +0300

    Update go plugin

--- lua/plugins/go.lua
+++ lua/plugins/go.lua
@@ -1,15 +1,31 @@
 return {
 	"ray-x/go.nvim",
-	dependencies = "ray-x/guihua.lua",
-	ft = "go",
+	dependencies = { -- optional packages
+		"ray-x/guihua.lua",
+		"neovim/nvim-lspconfig",
+		"nvim-treesitter/nvim-treesitter",
+	},
+	ft = { "go", "gomod" },
+	event = { "CmdlineEnter" },
 	init = function()
+		local format_sync_grp = vim.api.nvim_create_augroup("GoImport", {})
 		vim.api.nvim_create_autocmd("BufWritePre", {
-			pattern = { "*.go" },
-			group = vim.api.nvim_create_augroup("OrganizeImports", {}),
+			pattern = "*.go",
 			callback = function()
-				vim.cmd("GoImport")
+				require("go.format").goimport()
 			end,
+			group = format_sync_grp,
 		})
+
+		local present, lspconfig = pcall(require, "lspconfig")
+		if present then
+			lspconfig.gopls.setup({
+				gopls_cmd = { "gopls" },
+				fillstruct = "gopls",
+				dap_debug = true,
+				dap_debug_gui = true,
+			})
+		end
 	end,
 	config = function()
 		require("go").setup()
