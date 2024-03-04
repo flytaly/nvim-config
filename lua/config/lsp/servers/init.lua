4783cc6e29828f6f4166a28a5169e438c665e346
Vitaly Yerofeyevsky <flytaly@gmail.com>
2024-03-04 12:50:34 +0300

    Update Lua lsp config

--- lua/config/lsp/servers/init.lua
+++ lua/config/lsp/servers/init.lua
@@ -19,7 +19,29 @@ M.get_configs = function(on_attach, capabilities)
 				on_attach(client, bufnr)
 			end,
 		},
-		lua_ls = { settings = { Lua = { diagnostics = { globals = { "vim" } } } } },
+		lua_ls = {
+			settings = {
+				Lua = {
+					runtime = { version = "LuaJIT" },
+					workspace = {
+						checkThirdParty = false,
+						-- Tells lua_ls where to find all the Lua files that you have loaded
+						-- for your neovim configuration.
+						-- library = {
+						-- 	"${3rd}/luv/library",
+						-- 	unpack(vim.api.nvim_get_runtime_file("", true)),
+						-- },
+						-- If lua_ls is really slow on your computer, you can try this instead:
+						library = { vim.env.VIMRUNTIME },
+					},
+					completion = {
+						callSnippet = "Replace",
+					},
+					-- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
+					diagnostics = { disable = { "missing-fields" } },
+				},
+			},
+		},
 		html = { init_options = { provideFormatter = false } },
 		stylelint_lsp = {
 			root_dir = lspconfig.util.root_pattern(".stylelintrc", "stylelint.config.js", "package.json"),
