d9a2a460aae816bcd035bef6aee226a1aa8b6488
Vitaly Yerofeyevsky <flytaly@gmail.com>
2024-03-13 14:25:45 +0300

    Add sniprun

---
+++ lua/plugins/sniprun.lua
@@ -0,0 +1,18 @@
+return {
+	"michaelb/sniprun",
+	branch = "master",
+
+	build = "sh install.sh",
+	-- do 'sh install.sh 1' if you want to force compile locally
+	-- (instead of fetching a binary from the github release). Requires Rust >= 1.65
+	--
+	keys = {
+		{ "<leader>xr", ":'<,'>SnipRun<cr>", desc = "SnipRun", mode = "v" },
+	},
+
+	config = function()
+		require("sniprun").setup({
+			-- your options
+		})
+	end,
+}
