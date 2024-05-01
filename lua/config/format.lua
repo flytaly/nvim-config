171bafeef49ab7cdc2a6a6e417e22f532f7bc9b9
Vitaly Yerofeyevsky <flytaly@gmail.com>
2024-05-01 21:49:31 +0300

    Save/restore global variables to control format options

--- lua/config/format.lua
+++ lua/config/format.lua
@@ -1,27 +1,82 @@
+local saveData = require("config.save-data")
+
 local M = {}
 
-M.isEnabled = false
+local default_vars = {
+	format_on_save = true,
+	formatter = "",
+}
+
+local function updateVariables(update)
+	if update == nil then
+		return
+	end
+
+	if update["format_on_save"] ~= nil then
+		vim.g.format_on_save = update["format_on_save"]
+	end
+
+	if update["formatter"] ~= nil then
+		vim.g.formatter = update["formatter"]
+	end
+end
+
+local function getVariables()
+	return {
+		format_on_save = vim.g.format_on_save,
+		formatter = vim.g.formatter,
+	}
+end
+
+updateVariables(default_vars)
+
+vim.api.nvim_create_autocmd("VimEnter", {
+	group = vim.api.nvim_create_augroup("load_variables", {}),
+	callback = function()
+		local restoredData = saveData.load()
+		updateVariables(restoredData)
+	end,
+})
 
 vim.keymap.set("n", "yof", function()
-	M.isEnabled = not M.isEnabled
-	print("format on save", (M.isEnabled and "enabled" or "disabled"))
+	updateVariables({ format_on_save = not vim.g.format_on_save })
+	saveData.save(getVariables())
+	print("format on save", (vim.g.format_on_save and "enabled" or "disabled"))
 end, { desc = "Toggle format on save" })
 
-M.format = function()
+vim.api.nvim_create_user_command("FormatterSet", function(opts)
+	local formatter = opts.args or ""
+	updateVariables({ formatter = formatter })
+	saveData.save(getVariables())
+end, { desc = "Set formatter for the project", nargs = "?" })
+
+M.format = function(params)
+	params = params or { withMessage = true }
+
+	if vim.g.formatter == "eslint" and vim.fn.exists(":EslintFixAll") > 0 then
+		vim.cmd([[EslintFixAll]])
+		if params.withMessage then
+			print("ESLint Fix")
+		end
+		return
+	end
+
 	vim.cmd([[FormatWrite]])
-	print("format")
+	if params.withMessage then
+		print("formatted")
+	end
 end
 
 M.onSave = function(initial)
-	M.isEnabled = initial
+	vim.g.format_on_save = initial
 	local groupId = vim.api.nvim_create_augroup("__formatter__", { clear = true })
 	vim.api.nvim_create_autocmd("BufWritePost", {
 		group = groupId,
 		callback = function()
-			if not M.isEnabled or vim.bo.filetype == "go" then
+			if not vim.g.format_on_save or vim.bo.filetype == "go" then
 				return
 			end
-			vim.cmd([[FormatWrite]])
+			M.format({ withMessage = false })
 		end,
 	})
 end
---
+++ lua/config/save-data.lua
@@ -0,0 +1,73 @@
+local present, Path = pcall(require, "plenary.path")
+local M = {}
+
+if not present then
+	return {
+		load = function() end,
+		save = function() end,
+	}
+end
+
+-- store data in a json files (per project)
+-- based on https://github.com/ThePrimeagen/harpoon/blob/0378a6c428a0bed6a2781d459d7943843f374bce/lua/harpoon/data.lua
+--
+local data_path = string.format("%s/flytaly-cfg", vim.fn.stdpath("data"))
+local ensured_data_path = false
+
+local function ensure_data_path()
+	if ensured_data_path then
+		return
+	end
+
+	local path = Path:new(data_path)
+	if not path:exists() then
+		path:mkdir()
+	end
+	ensured_data_path = true
+end
+
+local function hash(path)
+	return vim.fn.sha256(path)
+end
+
+local function get_root_dir()
+	return vim.loop.cwd()
+end
+
+local function get_fullpath()
+	local h = hash(get_root_dir())
+	return string.format("%s/%s.json", data_path, h)
+end
+
+local function write_data(data)
+	ensure_data_path()
+	Path:new(get_fullpath()):write(vim.json.encode(data), "w")
+end
+
+local function read_data()
+	local path = Path:new(get_fullpath())
+	local exists = path:exists()
+
+	if not exists then
+		return {}
+	end
+
+	local out_data = path:read()
+
+	if not out_data or out_data == "" then
+		write_data({})
+		out_data = "{}"
+	end
+	local data = vim.json.decode(out_data)
+	return data
+end
+
+M.save = function(data)
+	write_data(data)
+end
+
+M.load = function()
+	return read_data()
+end
+
+return M
