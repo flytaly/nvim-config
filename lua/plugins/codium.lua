ef9343a40bc84c27e64329f4bc8e2e70a8fae4f6
Vitaly Yerofeyevsky <flytaly@gmail.com>
2024-09-07 03:38:52 +0300

    Codium: change ctrl to alt so it works in tmux

--- lua/plugins/codium.lua
+++ lua/plugins/codium.lua
@@ -3,16 +3,16 @@ return {
 	init = function()
 		vim.g.codeium_log_file = "~/.codeium/codeium.log"
 		vim.g.codeium_disable_bindings = 1
-		vim.keymap.set("i", "<c-.>", function()
+		vim.keymap.set("i", "<a-.>", function()
 			return vim.fn["codeium#CycleCompletions"](1)
 		end, { expr = true, desc = "Codeium: Go to next completion" })
-		vim.keymap.set("i", "<c-,>", function()
+		vim.keymap.set("i", "<a-,>", function()
 			return vim.fn["codeium#CycleCompletions"](-1)
 		end, { expr = true, desc = "Codeium: Go to previous completion" })
-		vim.keymap.set("i", "<c-x>", function()
+		vim.keymap.set("i", "<a-x>", function()
 			return vim.fn["codeium#Clear"]()
 		end, { expr = true, desc = "Codeium: Clear current completion" })
-		vim.keymap.set("i", "<c-cr>", function()
+		vim.keymap.set("i", "<a-cr>", function()
 			return vim.fn["codeium#Accept"]()
 		end, { expr = true, desc = "Codeium: Accept current completion" })
 	end,
