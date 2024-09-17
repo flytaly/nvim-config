7427edfee579921061fa45ff5cc88f5d673af5cf
Vitaly Yerofeyevsky <flytaly@gmail.com>
2024-09-17 12:05:44 +0300

    neo-tree: copy path

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,3 +1,44 @@
+-- https://github.com/nvim-neo-tree/neo-tree.nvim/discussions/370
+local function copy_path(state)
+	-- NeoTree is based on [NuiTree](https://github.com/MunifTanjim/nui.nvim/tree/main/lua/nui/tree)
+	-- The node is based on [NuiNode](https://github.com/MunifTanjim/nui.nvim/tree/main/lua/nui/tree#nuitreenode)
+	local node = state.tree:get_node()
+	local filepath = node:get_id()
+	local filename = node.name
+	local modify = vim.fn.fnamemodify
+
+	local results = {
+		filepath,
+		modify(filepath, ":."),
+		modify(filepath, ":~"),
+		filename,
+		modify(filename, ":r"),
+		modify(filename, ":e"),
+	}
+
+	vim.ui.select({
+		"1. Absolute path: " .. results[1],
+		"2. Path relative to CWD: " .. results[2],
+		"3. Path relative to HOME: " .. results[3],
+		"4. Filename: " .. results[4],
+		"5. Filename without extension: " .. results[5],
+		"6. Extension of the filename: " .. results[6],
+	}, { prompt = "Choose to copy to clipboard:" }, function(choice)
+		if choice then
+			local i = tonumber(choice:sub(1, 1))
+			if i then
+				local result = results[i]
+				vim.fn.setreg("+", result)
+				vim.notify("Copied: " .. result)
+			else
+				vim.notify("Invalid selection")
+			end
+		else
+			vim.notify("Selection cancelled")
+		end
+	end)
+end
+
 return {
 	{
 		-- refactor on file rename/move with some LSP
@@ -51,7 +92,13 @@ return {
 			--
 		},
 		config = function()
-			require("neo-tree").setup()
+			require("neo-tree").setup({
+				window = {
+					mappings = {
+						["Y"] = copy_path,
+					},
+				},
+			})
 		end,
 	},
 }
