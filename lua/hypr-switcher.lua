48ee21b246fafc660ac63b991c8f6379494db871
Vitaly Yerofeyevsky <flytaly@gmail.com>
2024-09-18 19:51:00 +0300

    Hyprland layout switcher update

--- lua/hypr-switcher.lua
+++ lua/hypr-switcher.lua
@@ -1,45 +1,79 @@
--- Hyperland keyboard layout switcher
+-- Hyprland keyboard layout switcher
 
 if os.getenv("HYPRLAND_INSTANCE_SIGNATURE") == nil then
 	return
 end
 
+local pkg = "HyprlXkbSwitcher"
+local group = vim.api.nvim_create_augroup("HyprlXkbSwitcher", { clear = true })
+
 local layouts = {
 	["English (US)"] = 0,
 	["Russian"] = 1,
 }
-local en_layout = 0
-local keyboard = "my-kmonad-output"
-local insert_layout = en_layout
 
-local function get_layout()
-	local output = vim.fn.system(
-		"hyprctl devices -j | jq -r '.keyboards[] | select(.name == \"" .. keyboard .. "\") | .active_keymap'   "
-	)
+local keyboard = "current" --"my-kmonad-output"
+local layout_normal = 0
+local layout_insert = layout_normal
 
-	output = output:gsub("\n", "")
-	return layouts[output] or 0
+local function get_keyboard(keyboards)
+	if keyboard == "current" then
+		for _, kb in ipairs(keyboards) do
+			if kb["main"] == true then
+				return kb
+			end
+		end
+	else
+		for _, kb in ipairs(keyboards) do
+			if kb["name"] == keyboard then
+				return kb
+			end
+		end
+	end
 end
 
-local group = vim.api.nvim_create_augroup("HyperKeyboardSwitcher", { clear = true })
+local function get_layout()
+	local output = vim.system({ "hyprctl", "devices", "-j" }, { text = true }):wait().stdout
+	if output == "" or output == nil then
+		return layout_normal
+	end
+
+	local devices = vim.json.decode(output)
+	local kb = get_keyboard(devices["keyboards"])
+	if kb == nil then
+		vim.notify_once(pkg .. " keyboard " .. keyboard .. " not found", vim.log.levels.WARN)
+		return layout_normal
+	end
+	local layout_name = kb["active_keymap"]
+
+	local layout = layouts[layout_name]
+	if layout == nil then
+		vim.notify_once(pkg .. " unknown layout name: " .. layout_name, vim.log.levels.WARN)
+		return layout_normal
+	end
+	return layout
+end
 
 vim.api.nvim_create_autocmd("InsertLeavePre", {
 	callback = function()
-		insert_layout = get_layout()
-		if insert_layout == en_layout then
+		local ok, result = pcall(get_layout)
+		if not ok then
+			vim.notify_once(pkg .. " error: " .. result, vim.log.levels.ERROR)
 			return
 		end
-		vim.fn.system("hyprctl switchxkblayout " .. keyboard .. " " .. en_layout)
+		layout_insert = result
+		if layout_insert ~= layout_normal then
+			vim.fn.system("hyprctl switchxkblayout " .. keyboard .. " " .. layout_normal)
+		end
 	end,
 	group = group,
 })
 
 vim.api.nvim_create_autocmd("InsertEnter", {
 	callback = function()
-		if insert_layout == en_layout then
-			return
+		if layout_insert ~= layout_normal then
+			vim.fn.system("hyprctl switchxkblayout " .. keyboard .. " " .. layout_insert)
 		end
-		vim.fn.system("hyprctl switchxkblayout " .. keyboard .. " " .. insert_layout)
 	end,
 	group = group,
 })
