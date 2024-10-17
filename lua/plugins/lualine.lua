358fd55a9240bef6828ddf4c8a6ff22b28e7d6e4
Vitaly Yerofeyevsky <flytaly@gmail.com>
2024-10-18 01:18:10 +0300

    Lualine change separators and show arrow status

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,6 +2,35 @@ return {
 	"nvim-lualine/lualine.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	config = function()
-		require("lualine").setup({})
+		local ok, statusline = pcall(require, "arrow.statusline")
+
+		local function arrow()
+			if ok then
+				return statusline.text_for_statusline_with_icons()
+			end
+		end
+
+		require("lualine").setup({
+			options = {
+				component_separators = { left = "", right = "" },
+				section_separators = { left = "", right = "" },
+			},
+			sections = {
+				lualine_a = { "mode" },
+				lualine_b = { "branch", "diff", "diagnostics" },
+				lualine_c = { arrow, "filename" },
+				lualine_x = { "encoding", "fileformat", "filetype" },
+				lualine_y = { "progress" },
+				lualine_z = { "location" },
+			},
+			inactive_sections = {
+				lualine_a = {},
+				lualine_b = {},
+				lualine_c = { "filename" },
+				lualine_x = { "location" },
+				lualine_y = {},
+				lualine_z = {},
+			},
+		})
 	end,
 }
