d867f4ca18de651d52d05e0e500cb5c63f47d832
Vitaly Yerofeyevsky <flytaly@nice-boat.>
2024-11-05 17:41:15 +0300

    focus.nvim fix

--- lua/plugins/zen-mode.lua
+++ lua/plugins/zen-mode.lua
@@ -2,10 +2,12 @@ return {
 	{
 		"cdmill/focus.nvim",
 		cmd = { "Focus", "Zen", "Narrow" },
+		event = "VeryLazy",
 		keys = {
 			{ "<leader>mf", ":Focus<CR>", desc = "Focus: focus mode" },
 			{ "<leader>mz", ":Zen<CR>", desc = "Focus: zen mode" },
 			{ "<leader>mn", ":Narrow<CR>", mode = { "v", "n" }, desc = "Focus: narrow mode" },
 		},
+		opts = {},
 	},
 }
