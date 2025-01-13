bd4b4f501c521b5ad9e49c8fb26002a23c0a8aeb
Vitaly Yerofeyevsky <flytaly@gmail.com>
2025-01-13 22:18:43 +0300

    nvim-cmp: enable c-y mapping

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -100,12 +100,12 @@ return {
 					ls.lsp_expand(args.body)
 				end,
 			},
-
 			mapping = {
 				["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
 				["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
 				["<C-Space>"] = cmp.mapping({ i = toggle_popup, c = toggle_popup }),
-				["<C-y>"] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
+				["<C-y>"] = cmp.mapping({ i = toggle_popup, c = toggle_popup }),
+				-- ["<C-y>"] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
 				["<C-e>"] = cmp.mapping({ i = cmp.mapping.abort(), c = cmp.mapping.close() }),
 				["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
 				["<C-n>"] = cmp.mapping(select_next()),
