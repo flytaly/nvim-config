d4bba616bc17d65face9721d0115dde7a00c18a9
Vitaly Yerofeyevsky <flytaly@gmail.com>
2025-02-27 19:43:57 +0300

    Treesitter: disable highlighting for big files

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -12,10 +12,22 @@ return {
 	config = function()
 		require("nvim-treesitter.configs").setup({
 			ensure_installed = "all",
-		    ignore_install = { "hlsplaylist", "muttrc", "zathurarc" },
+			ignore_install = { "hlsplaylist", "muttrc", "zathurarc" },
 
 			highlight = {
 				enable = true,
+				disable = function(lang, buf)
+					local max_filesize = 100 * 1024 -- 100 KB
+					local js_max_filesize = 50 * 1024 -- 50 KB
+					local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+					if
+						ok
+						and stats
+						and (stats.size > max_filesize or lang == "js" and stats.size > js_max_filesize)
+					then
+						return true
+					end
+				end,
 			},
 			indent = {
 				enable = true,
