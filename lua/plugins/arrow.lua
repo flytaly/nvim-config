3970ed33fe962de01a5b8b0abe26f884c27042c1
Vitaly Yerofeyevsky <flytaly@gmail.com>
2025-03-07 03:56:42 +0300

    arrow.nvim: use w,q instead of ],[

--- lua/plugins/arrow.lua
+++ lua/plugins/arrow.lua
@@ -4,5 +4,17 @@ return {
 		show_icons = true,
 		leader_key = "'", -- Recommended to be a single key
 		buffer_leader_key = "m", -- Per Buffer Mappings
+		mappings = {
+			edit = "e",
+			delete_mode = "d",
+			clear_all_items = "C",
+			toggle = "s", -- used as save if separate_save_and_remove is true
+			open_vertical = "v",
+			open_horizontal = "-",
+			quit = "Q",
+			remove = "x", -- only used if separate_save_and_remove is true
+			next_item = "w",
+			prev_item = "q",
+		},
 	},
 }
