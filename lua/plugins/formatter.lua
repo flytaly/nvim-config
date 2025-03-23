26e762b31dccd6b4c770309fccb2749121191ad3
Vitaly Yerofeyevsky <flytaly@gmail.com>
2025-03-23 19:54:20 +0300

    formatter: import stylua, report if formatter isn't available
    Stylua formatter is located only in filetypes file.

--- lua/plugins/formatter.lua
+++ lua/plugins/formatter.lua
@@ -4,6 +4,7 @@ return {
 		local get_overridden_formatter = require("config.format").get_overridden_formatter
 		local formatter_list = require("config.format").formatter_list
 		local formatters = require("formatter.defaults")
+		formatters["stylua"] = require("formatter.filetypes.lua").stylua
 
 		-- select formatter in runtime
 		local function select(filetype)
@@ -15,7 +16,11 @@ return {
 						selected = override
 					end
 				end
-				return formatters[selected] and formatters[selected]()
+				if formatters[selected] == nil then
+					vim.notify('formatter "' .. selected .. '" not found', vim.log.levels.WARN)
+					return
+				end
+				return formatters[selected]()
 			end
 		end
 
