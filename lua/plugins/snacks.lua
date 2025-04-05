51453d12cebb18b5d50250f27494225a673cab89
Vitaly Yerofeyevsky <flytaly@gmail.com>
2025-04-05 23:52:05 +0300

    Use snacks instead of telescope for most of the pickers and searches

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -24,54 +24,100 @@ return {
 			statuscolumn = { enabled = true },
 			lazygit = { enabled = true },
 			words = { enabled = true },
+			terminal = {
+				bo = {
+					filetype = "snacks_terminal",
+				},
+				wo = {},
+				keys = {
+					q = "hide",
+					gf = function(self)
+						local f = vim.fn.findfile(vim.fn.expand("<cfile>"), "**")
+						if f == "" then
+							Snacks.notify.warn("No file under cursor")
+						else
+							self:hide()
+							vim.schedule(function() vim.cmd("e " .. f) end)
+						end
+					end,
+					term_normal = {
+						"<esc>",
+						function(self)
+							self.esc_timer = self.esc_timer or (vim.uv or vim.loop).new_timer()
+							if self.esc_timer:is_active() then
+								self.esc_timer:stop()
+								vim.cmd("stopinsert")
+							else
+								self.esc_timer:start(200, 0, function() end)
+								return "<esc>"
+							end
+						end,
+						mode = "t",
+						expr = true,
+						desc = "Double escape to normal mode",
+					},
+				},
+			},
 		},
 		keys = {
+			-- Top Pickers & Explorer
+			{ "<leader><space>", function() Snacks.picker.smart() end, desc = "Smart Find Files" },
+			{ "<leader>b", function() Snacks.picker.buffers() end, desc = "Buffers" },
+			{ "<leader>/", function() Snacks.picker.grep() end, desc = "Grep" },
+			{ "<leader>:", function() Snacks.picker.command_history() end, desc = "Command History" },
+			{ "<leader>nn", function() Snacks.picker.notifications() end, desc = "Notification History" },
+			{ "<leader>e", function() Snacks.explorer() end, desc = "File Explorer" },
+			-- find
+			{ "<leader>fb", function() Snacks.picker.buffers() end, desc = "Buffers" },
+			{ "<leader>ff", function() Snacks.picker.files() end, desc = "Find Files" },
+			{ "<leader>fg", function() Snacks.picker.git_files() end, desc = "Find Git Files" },
+			{ "<leader>fp", function() Snacks.picker.projects() end, desc = "Projects" },
+			{ "<leader>fr", function() Snacks.picker.recent() end, desc = "Recent" },
+			-- Grep
+			{ "<leader>sb", function() Snacks.picker.lines() end, desc = "Buffer Lines" },
+			{ "<leader>sB", function() Snacks.picker.grep_buffers() end, desc = "Grep Open Buffers" },
+			{ "<leader>sg", function() Snacks.picker.grep() end, desc = "Grep" },
 			{
-				"<leader>:",
-				function() Snacks.picker.command_history() end,
-				desc = "Command History",
+				"<leader>sw",
+				function() Snacks.picker.grep_word() end,
+				desc = "Visual selection or word",
+				mode = { "n", "x" },
 			},
-			{
-				"<leader>nn",
-				function() Snacks.picker.notifications() end,
-				desc = "Notification History",
-			},
-			{
-				"<leader>si",
-				function() Snacks.picker.icons() end,
-				desc = "Icons",
-			},
-			-- Other
-			{
-				"<leader>.",
-				function() Snacks.scratch() end,
-				desc = "Toggle Scratch Buffer",
-			},
-			{
-				"<leader>>",
-				function() Snacks.scratch.select() end,
-				desc = "Select Scratch Buffer",
-			},
-			{
-				"<leader>nn",
-				function() Snacks.notifier.show_history() end,
-				desc = "Notification History",
-			},
-			{
-				"<leader>bd",
-				function() Snacks.bufdelete() end,
-				desc = "Delete Buffer",
-			},
-			{
-				"<leader>gl",
-				function() Snacks.lazygit() end,
-				desc = "Lazygit",
-			},
-			{
-				"<leader>nh",
-				function() Snacks.notifier.hide() end,
-				desc = "Dismiss All Notifications",
-			},
+			-- search
+			{ '<leader>s"', function() Snacks.picker.registers() end, desc = "Registers" },
+			{ "<leader>s/", function() Snacks.picker.search_history() end, desc = "Search History" },
+			{ "<leader>sa", function() Snacks.picker.autocmds() end, desc = "Autocmds" },
+			{ "<leader>sb", function() Snacks.picker.lines() end, desc = "Buffer Lines" },
+			{ "<leader>sc", function() Snacks.picker.command_history() end, desc = "Command History" },
+			{ "<leader>sC", function() Snacks.picker.commands() end, desc = "Commands" },
+			{ "<leader>sd", function() Snacks.picker.diagnostics() end, desc = "Diagnostics" },
+			{ "<leader>sD", function() Snacks.picker.diagnostics_buffer() end, desc = "Buffer Diagnostics" },
+			{ "<leader>sh", function() Snacks.picker.help() end, desc = "Help Pages" },
+			{ "<leader>sH", function() Snacks.picker.highlights() end, desc = "Highlights" },
+			{ "<leader>si", function() Snacks.picker.icons() end, desc = "Icons" },
+			{ "<leader>sj", function() Snacks.picker.jumps() end, desc = "Jumps" },
+			{ "<leader>sk", function() Snacks.picker.keymaps() end, desc = "Keymaps" },
+			{ "<leader>sl", function() Snacks.picker.loclist() end, desc = "Location List" },
+			{ "<leader>sm", function() Snacks.picker.marks() end, desc = "Marks" },
+			{ "<leader>sM", function() Snacks.picker.man() end, desc = "Man Pages" },
+			{ "<leader>sp", function() Snacks.picker.lazy() end, desc = "Search for Plugin Spec" },
+			{ "<leader>sq", function() Snacks.picker.qflist() end, desc = "Quickfix List" },
+			{ "<leader>sR", function() Snacks.picker.resume() end, desc = "Resume" },
+			{ "<leader>su", function() Snacks.picker.undo() end, desc = "Undo History" },
+			-- LSP
+			{ "<leader>ss", function() Snacks.picker.lsp_symbols() end, desc = "LSP Symbols" },
+			{ "<leader>sS", function() Snacks.picker.lsp_workspace_symbols() end, desc = "LSP Workspace Symbols" },
+
+			{ "<leader>.", function() Snacks.scratch() end, desc = "Toggle Scratch Buffer" },
+			{ "<leader>>", function() Snacks.scratch.select() end, desc = "Select Scratch Buffer" },
+			{ "<leader>bd", function() Snacks.bufdelete() end, desc = "Delete Buffer" },
+			{ "<leader>gl", function() Snacks.lazygit() end, desc = "Lazygit" },
+			{ "<leader>nh", function() Snacks.notifier.hide() end, desc = "Dismiss All Notifications" },
+			{ "<c-\\>", function() Snacks.terminal() end, mode = { "n", "t", "i" }, desc = "Toggle Terminal" },
+			{ "<C-h>", "[[<Cmd>wincmd h<CR>]]", mode = "t" },
+			{ "<C-j>", "[[<Cmd>wincmd j<CR>]]", mode = "t" },
+			{ "<C-k>", "[[<Cmd>wincmd k<CR>]]", mode = "t" },
+			{ "<C-l>", "[[<Cmd>wincmd l<CR>]]", mode = "t" },
 		},
 	},
 }
