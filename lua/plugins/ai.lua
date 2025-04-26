1502ac2c230a6e253369436642ce7b76823f5eda
Vitaly Yerofeyevsky <flytaly@gmail.com>
2025-04-26 15:39:18 +0300

    Neocodeium: add command to enable in the buffer

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -27,6 +27,11 @@ return {
 			vim.keymap.set("i", "<a-,>", function() require("neocodeium").cycle_or_complete() end)
 			vim.keymap.set("i", "<a-.>", function() require("neocodeium").cycle_or_complete(-1) end)
 			vim.keymap.set("i", "<a-C>", function() require("neocodeium").clear() end)
+
+			vim.api.nvim_create_user_command("NeoCodeiumEnableBuffer", function()
+				vim.cmd("NeoCodeium enable")
+				vim.cmd("NeoCodeium enable_buffer")
+			end, { desc = "Start neocodeium and enable for current buffer" })
 		end,
 	},
 	-- {
