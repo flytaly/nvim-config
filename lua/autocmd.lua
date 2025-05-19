8b5ef4a2c9f376c36088bc06b38a50487e0d8175
Vitaly Yerofeyevsky <flytaly@gmail.com>
2025-05-19 13:21:27 +0300

    ref: autocmd

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -3,11 +3,18 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 	callback = function() vim.highlight.on_yank({ timeout = 200 }) end,
 })
 
--- Folds
--- vim.cmd([[
--- augroup remember_folds
---   autocmd!
---   autocmd BufWinLeave *.* mkview
---   autocmd BufWinEnter *.* silent! loadview
--- augroup END
--- ]])
+vim.api.nvim_create_autocmd("BufEnter", {
+	group = vim.api.nvim_create_augroup("restore_folds", { clear = true }),
+	callback = function()
+		if vim.b.was_loaded then
+			return
+		end
+		vim.b.was_loaded = true
+		vim.cmd([[silent! loadview]])
+	end,
+})
+
+vim.api.nvim_create_autocmd("BufWinLeave", {
+	group = vim.api.nvim_create_augroup("save_folds", { clear = true }),
+	callback = function() vim.cmd([[silent! mkview]]) end,
+})
