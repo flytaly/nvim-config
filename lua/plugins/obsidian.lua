e912a09beb798b6768b5adf86f06ea47aa6913b1
Vitaly Yerofeyevsky <flytaly@gmail.com>
2025-11-25 00:33:56 +0300

    fix(windows): disable obsidian

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -1,6 +1,7 @@
 return {
 	"obsidian-nvim/obsidian.nvim",
 	version = "*", -- recommended, use latest release instead of latest commit
+	enabled = vim.loop.os_uname().sysname ~= "Windows_NT",
 	lazy = true,
 	ft = "markdown",
 	-- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
